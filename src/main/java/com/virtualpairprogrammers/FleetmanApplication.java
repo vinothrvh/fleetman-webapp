69fd0e99e13a43432c6baf6ce6a60e9ed61a23e4
vinoth47 <vinothrvh@gmail.com>
2020-06-12 10:23:03 +0530

    Push and commit from local to remote

--- src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//Push and commit from local to remote
 //Main App
 @SpringBootApplication
 @EnableScheduling
